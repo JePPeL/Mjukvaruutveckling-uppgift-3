d1355773746570bd4826040b7d7ec1c4fa573707
Tonfisk <Woodie@Eriks-MacBook-Pro.local>
2018-01-03 15:55:28 +0100

    contorller

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,15 +1,21 @@
 package main;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class Controller {
 	private CustomerRegister cReg = new CustomerRegister();
+	private int maxNbr = 10000000;
+	private List<String> idList = new LinkedList<String>();
 	
-	public boolean addCustomer(String name, String adress) {
-		cReg.addCustomer(new Customer(custNumbGen(), name, adress))
+	public void addCustomer(String name, String adress) {
+		cReg.addCustomer(new Customer(custNumbGen(), name, adress));
 		
 	}
 	
-	public Custumer getCustomer() {
-		return cReg.findCustomer(customerNumber);
+	public Customer getCustomer(String CustomerNumber) {
+		return cReg.findCustomer(CustomerNumber);
 	}
 	
 	public void removeCustomer() {
@@ -19,9 +25,11 @@ public class Controller {
 	
 	public String custNumbGen() {
 		Random rand = new Random();
+		
 		String random = Integer.toString(rand.nextInt(maxNbr));
 		boolean b = true;
 		while (b) {
+			String idList;
 			if (idList.contains(random)) {
 				random = Integer.toString(rand.nextInt(maxNbr));
 			} else
