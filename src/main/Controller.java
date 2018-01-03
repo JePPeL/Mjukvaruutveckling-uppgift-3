12e2b1432152ab5b9875a043c332760d979ee5ec
Tonfisk <Woodie@Eriks-MacBook-Pro.local>
2018-01-03 15:42:51 +0100

    add customer

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,5 +1,26 @@
 package main;
 
 public class Controller {
-
+	private CustomerRegister cReg = new CustomerRegister();
+	
+	public boolean addCustomer(String name, String adress) {
+		cReg.addCustomer(new Customer(custNumbGen(), name, adress))
+		
+	}
+	
+	public String custNumbGen() {
+		Random rand = new Random();
+		String random = Integer.toString(rand.nextInt(maxNbr));
+		boolean b = true;
+		while (b) {
+			if (idList.contains(random)) {
+				random = Integer.toString(rand.nextInt(maxNbr));
+			} else
+				idList.add(random);
+			b = false;
+		}
+		return random;
+	}
+	
+	
 }
