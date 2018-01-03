031b593d313ca0938a036f04fb1459c30350cc22
JesperL <dat14jlu@student.lu.se>
2018-01-03 17:06:59 +0100

    other stuff

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -11,53 +11,13 @@ public class Controller {
 	//Start Customer & Customer Register
 	private GUI view;
 	private Model model;
-	private int maxNbr = 10000000;
-	private List<String> idList = new LinkedList<String>();
-	private String CustomerNumber;
 	
 	public Controller(GUI view, Model model) {
 		this.view=view;
 		this.model=model;
 	}
 	
-	public void addCustomer(String name, String address) {
-		cReg.addCustomer(new Customer(custNumbGen(), name, address));
-		
-	}
-	
-	public Customer findCustomer(String cNbr) {
-		if (cReg.findCustomer(cNbr) != null) {
-			this.CustomerNumber = cReg.findCustomer(cNbr).getCustomerNumber();
-			return cReg.findCustomer(cNbr);
-		} else {
-			return null;
-		}
-	}
-	
-	public Customer getCustomer() {
-		return cReg.findCustomer(CustomerNumber);
-	}
-	
-	public void removeCustomer() {
-		cReg.removeCustomer(CustomerNumber);
-	}
-	
-	
-	public String custNumbGen() {
-		Random rand = new Random();
-		
-		String random = Integer.toString(rand.nextInt(maxNbr));
-		boolean b = true;
-		while (b) {
-	
-			if (idList.contains(random)) {
-				random = Integer.toString(rand.nextInt(maxNbr));
-			} else
-				idList.add(random);
-			b = false;
-		}
-		return random;
-	}
+
 	
 	//End Customer & CustomerRegister
 }
