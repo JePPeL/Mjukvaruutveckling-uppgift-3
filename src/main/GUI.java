2e4b93c4bb8fa506afd42f2ac896b01a889b8d2e
Tonfisk <Woodie@Eriks-MacBook-Pro.local>
2018-01-03 16:30:36 +0100

    gui

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -7,6 +7,7 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class GUI {
 
@@ -15,7 +16,6 @@ public class GUI {
 	private JTextField fieldProductID;
 	private JTextField fieldOrderID;
 	private JTextField fieldOrderLineID;
-	private JTextField fieldOrderLineAmount;
 	private JTextField fieldAmountInventory;
 	private JTextField fieldAmountOrderLine;
 	private JTextField fieldName;
@@ -49,30 +49,30 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 700, 300);
+		frame.setBounds(100, 100, 700, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		fieldCustomerID = new JTextField();
-		fieldCustomerID.setBounds(10, 57, 287, 20);
+		fieldCustomerID.setBounds(12, 73, 287, 20);
 		frame.getContentPane().add(fieldCustomerID);
 		fieldCustomerID.setColumns(10);
 		
 		fieldProductID = new JTextField();
 		fieldProductID.setColumns(10);
-		fieldProductID.setBounds(10, 150, 287, 20);
+		fieldProductID.setBounds(10, 269, 287, 20);
 		frame.getContentPane().add(fieldProductID);
 		
 		JButton btnAddProduct = new JButton("L\u00E4gg till");
-		btnAddProduct.setBounds(109, 179, 89, 23);
+		btnAddProduct.setBounds(109, 298, 89, 23);
 		frame.getContentPane().add(btnAddProduct);
 		
 		JButton btnSearchProduct = new JButton("S\u00F6k");
-		btnSearchProduct.setBounds(10, 179, 89, 23);
+		btnSearchProduct.setBounds(10, 298, 89, 23);
 		frame.getContentPane().add(btnSearchProduct);
 		
 		JButton btnAddCustomer = new JButton("L\u00E4gg till");
-		btnAddCustomer.setBounds(109, 88, 89, 23);
+		btnAddCustomer.setBounds(111, 104, 89, 23);
 		frame.getContentPane().add(btnAddCustomer);
 		
 		JButton btnSearchCustomer = new JButton("S\u00F6k");
@@ -80,72 +80,67 @@ public class GUI {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnSearchCustomer.setBounds(10, 88, 89, 23);
+		btnSearchCustomer.setBounds(12, 104, 89, 23);
 		frame.getContentPane().add(btnSearchCustomer);
 		
 		fieldOrderID = new JTextField();
-		fieldOrderID.setBounds(362, 11, 267, 20);
+		fieldOrderID.setBounds(359, 73, 267, 20);
 		frame.getContentPane().add(fieldOrderID);
 		fieldOrderID.setColumns(10);
 		
 		JButton btnAddOrder = new JButton("L\u00E4gg Till");
-		btnAddOrder.setBounds(455, 42, 89, 23);
+		btnAddOrder.setBounds(452, 104, 89, 23);
 		frame.getContentPane().add(btnAddOrder);
 		
 		fieldOrderLineID = new JTextField();
-		fieldOrderLineID.setBounds(362, 146, 86, 20);
+		fieldOrderLineID.setBounds(359, 269, 267, 20);
 		frame.getContentPane().add(fieldOrderLineID);
 		fieldOrderLineID.setColumns(10);
 		
 		JButton btnRemoveCustomer = new JButton("Ta bort");
-		btnRemoveCustomer.setBounds(208, 88, 89, 23);
+		btnRemoveCustomer.setBounds(210, 104, 89, 23);
 		frame.getContentPane().add(btnRemoveCustomer);
 		
 		JButton btnRemoveProduct = new JButton("Ta bort");
-		btnRemoveProduct.setBounds(208, 178, 89, 23);
+		btnRemoveProduct.setBounds(208, 297, 89, 23);
 		frame.getContentPane().add(btnRemoveProduct);
 		
 		JButton btnSearchOrder = new JButton("S\u00F6k");
-		btnSearchOrder.setBounds(362, 42, 89, 23);
+		btnSearchOrder.setBounds(359, 104, 89, 23);
 		frame.getContentPane().add(btnSearchOrder);
 		
 		JButton btnRemoveOrder = new JButton("Ta bort");
-		btnRemoveOrder.setBounds(554, 42, 89, 23);
+		btnRemoveOrder.setBounds(551, 104, 89, 23);
 		frame.getContentPane().add(btnRemoveOrder);
 		
 		JButton btnSearchOrderLine = new JButton("S\u00F6k");
-		btnSearchOrderLine.setBounds(359, 178, 89, 23);
+		btnSearchOrderLine.setBounds(359, 298, 89, 23);
 		frame.getContentPane().add(btnSearchOrderLine);
 		
-		fieldOrderLineAmount = new JTextField();
-		fieldOrderLineAmount.setBounds(455, 146, 156, 20);
-		frame.getContentPane().add(fieldOrderLineAmount);
-		fieldOrderLineAmount.setColumns(10);
-		
 		JButton btnLggTill = new JButton("L\u00E4gg till");
-		btnLggTill.setBounds(455, 178, 89, 23);
+		btnLggTill.setBounds(452, 301, 89, 23);
 		frame.getContentPane().add(btnLggTill);
 		
 		JButton btnTaBort = new JButton("Ta bort");
-		btnTaBort.setBounds(554, 178, 89, 23);
+		btnTaBort.setBounds(551, 301, 89, 23);
 		frame.getContentPane().add(btnTaBort);
 		
 		fieldAmountInventory = new JTextField();
-		fieldAmountInventory.setBounds(10, 212, 179, 20);
+		fieldAmountInventory.setBounds(14, 351, 179, 20);
 		frame.getContentPane().add(fieldAmountInventory);
 		fieldAmountInventory.setColumns(10);
 		
 		JButton btnRemoveFromInventory = new JButton("Ta bort");
-		btnRemoveFromInventory.setBounds(109, 243, 89, 23);
+		btnRemoveFromInventory.setBounds(113, 382, 89, 23);
 		frame.getContentPane().add(btnRemoveFromInventory);
 		
 		JButton btnAddToInventory = new JButton("L\u00E4gg till");
-		btnAddToInventory.setBounds(10, 243, 89, 23);
+		btnAddToInventory.setBounds(14, 382, 89, 23);
 		frame.getContentPane().add(btnAddToInventory);
 		
 		fieldAmountOrderLine = new JTextField();
 		fieldAmountOrderLine.setColumns(10);
-		fieldAmountOrderLine.setBounds(356, 212, 179, 20);
+		fieldAmountOrderLine.setBounds(360, 355, 179, 20);
 		frame.getContentPane().add(fieldAmountOrderLine);
 		
 		JButton btnRemoveFromOrderLine = new JButton("Ta bort");
@@ -153,21 +148,53 @@ public class GUI {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnRemoveFromOrderLine.setBounds(455, 243, 89, 23);
+		btnRemoveFromOrderLine.setBounds(459, 386, 89, 23);
 		frame.getContentPane().add(btnRemoveFromOrderLine);
 		
 		JButton btnAddToOrderLine = new JButton("L\u00E4gg till");
-		btnAddToOrderLine.setBounds(356, 243, 89, 23);
+		btnAddToOrderLine.setBounds(360, 386, 89, 23);
 		frame.getContentPane().add(btnAddToOrderLine);
 		
 		fieldName = new JTextField();
-		fieldName.setBounds(10, 26, 86, 20);
+		fieldName.setBounds(10, 26, 137, 20);
 		frame.getContentPane().add(fieldName);
 		fieldName.setColumns(10);
 		
 		fieldAddress = new JTextField();
-		fieldAddress.setBounds(112, 26, 86, 20);
+		fieldAddress.setBounds(164, 26, 122, 20);
 		frame.getContentPane().add(fieldAddress);
 		fieldAddress.setColumns(10);
+		
+		JLabel lblKundnummer = new JLabel("Kundnummer");
+		lblKundnummer.setBounds(12, 7, 124, 16);
+		frame.getContentPane().add(lblKundnummer);
+		
+		JLabel lblNamn = new JLabel("Namn");
+		lblNamn.setBounds(164, 7, 61, 16);
+		frame.getContentPane().add(lblNamn);
+		
+		JLabel lblAdress = new JLabel("Adress");
+		lblAdress.setBounds(15, 53, 61, 16);
+		frame.getContentPane().add(lblAdress);
+		
+		JLabel lblProduktnummer = new JLabel("Produktnummer");
+		lblProduktnummer.setBounds(12, 248, 113, 16);
+		frame.getContentPane().add(lblProduktnummer);
+		
+		JLabel lblOrdernummer = new JLabel("Ordernummer");
+		lblOrdernummer.setBounds(359, 53, 113, 16);
+		frame.getContentPane().add(lblOrdernummer);
+		
+		JLabel lblOrderrad = new JLabel("Orderrad");
+		lblOrderrad.setBounds(359, 248, 61, 16);
+		frame.getContentPane().add(lblOrderrad);
+		
+		JLabel lblNewLabel = new JLabel("Antal");
+		lblNewLabel.setBounds(17, 328, 61, 16);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Antal");
+		lblNewLabel_1.setBounds(364, 332, 61, 16);
+		frame.getContentPane().add(lblNewLabel_1);
 	}
 }
