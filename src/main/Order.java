3ebb3d6c92704e924fa6ba8f878eb3d61919ca1f
JesperL <dat14jlu@student.lu.se>
2018-01-09 14:32:05 +0100

    fixx

--- src/main/Order.java
+++ src/main/Order.java
@@ -5,15 +5,12 @@ import java.util.HashMap;
 
 public class Order {
 	private String orderID;
-	//NB: Shouldn't change, don't create setter
-	private Customer customer;
 	//Changed customer to be immutable, seems cleaner to me /Jesper
 	private String deliveryDate;
 	private HashMap<String, OrderLine> orderLines  = new HashMap<String, OrderLine>();
 	
-	public Order(String orderID, Customer customer, String deliveryDate) {
+	public Order(String orderID, String deliveryDate) {
 		this.orderID = orderID;
-		this.customer = customer;
 		this.deliveryDate = deliveryDate;
 	}
 	
@@ -24,10 +21,6 @@ public class Order {
 
 	public String getOrderID() {
 		return orderID;
-	}
-
-	public Customer getCustomer() {
-		return customer;
 	}
 
 	public String getDeliveryDate() {
